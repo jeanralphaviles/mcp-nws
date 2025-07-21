7cbb7ca1699c52f21e4eca4d6c6f74987914fa91
Jean-Ralph Aviles <jeanralph.aviles@gmail.com>
2025-07-20 18:04:21 -0700

    mcp-nws: Initial Commit
    Contains an MCP Server with a single tool, Forecast, for retrieving a
    simple forecast for a point (lat/long) within the United States from the
    National Weather Service API.

---
+++ cmd/mcp-nws/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/icodealot/noaa"
+	"github.com/modelcontextprotocol/go-sdk/mcp"
+)
+
+var (
+	httpAddr = flag.String("address", "", "Address to listen on. If not set, run MCP Server in STDIO mode.")
+)
+
+// ForecastParams are arguments for mcp-nws calls. They encode the latitude and
+// longitude to obtain a forecast for.
+type ForecastParams struct {
+	Latitude  string `json:"latitude"`
+	Longitude string `json:"longitude"`
+}
+
+// ForecastResult describes the return type of the Forecast tool.
+type ForecastResult = noaa.ForecastResponse
+
+// Forecast returns a standard weather forecast for a location covering 14 periods (day and night for 7 days).
+func Forecast(ctx context.Context, ss *mcp.ServerSession, params *mcp.CallToolParamsFor[ForecastParams]) (*mcp.CallToolResultFor[ForecastResult], error) {
+	var res mcp.CallToolResultFor[ForecastResult]
+
+	forecast, err := noaa.Forecast(params.Arguments.Latitude, params.Arguments.Longitude)
+	if err != nil {
+		return nil, err
+	}
+	res.StructuredContent = *forecast
+
+	return &res, nil
+}
+
+func main() {
+	flag.Parse()
+
+	server := mcp.NewServer(&mcp.Implementation{Name: "mcp-nws", Title: "US National Weather Service MCP Server", Version: "v1.0.0"}, nil)
+	mcp.AddTool(server, &mcp.Tool{Name: "Forecast", Description: "7d Weather Forecast"}, Forecast)
+
+	if *httpAddr != "" {
+		handler := mcp.NewStreamableHTTPHandler(func(*http.Request) *mcp.Server {
+			return server
+		}, nil)
+		log.Printf("MCP handler listening at %s", *httpAddr)
+		if err := http.ListenAndServe(*httpAddr, handler); err != nil {
+			log.Fatalf("HTTP server failed: %v", err)
+		}
+	} else {
+		t := mcp.NewLoggingTransport(mcp.NewStdioTransport(), os.Stderr)
+		if err := server.Run(context.Background(), t); err != nil {
+			log.Fatalf("Server failed: %s", err)
+		}
+	}
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/jeanralphaviles/mcp-nws
+
+go 1.23.5
+
+require github.com/modelcontextprotocol/go-sdk v0.2.0
+
+require (
+	github.com/icodealot/noaa v0.0.2
+	github.com/yosida95/uritemplate/v3 v3.0.2 // indirect
+)
